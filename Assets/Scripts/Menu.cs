30574599dfc3e72809aaa0f2041fb274cba8af17
tomual <tomual@users.noreply.github.com>
2019-01-09 16:22:52 +0800

    Menu scene, start on play click

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public void StartClick()
+    {
+        SceneManager.LoadScene("World");
+    }
+
+    public void QuitClick()
+    {
+        Application.Quit();
+    }
+}
