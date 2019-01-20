c73dc2c0b1a59c2fd93c88c62984de43b00e4d07
tomual <tomual@users.noreply.github.com>
2019-01-20 22:34:33 +0800

    Boss init

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : Enemy {
+
+    private bool allowedToMove = false;
+
+    void Update()
+    {
+        if (isDead())
+        {
+            Die();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (allowedToMove)
+        {
+        }
+    }
+
+    public void enableBoss(bool enable)
+    {
+        allowedToMove = enable;
+    }
+}
