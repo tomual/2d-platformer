ccf9d4a688a8b4791479c0cffdf60b962b1174e5
tomual <tomual@users.noreply.github.com>
2019-03-08 21:17:28 +0800

    Player jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Animator animator;
-    Rigidbody2D rigidbody;
-    bool facingRight = true;
+    private Animator animator;
+    private Rigidbody2D rigidbody;
+    private bool facingRight = true;
+    private Transform groundCheck;
+    private bool jump;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody2D>();
+        groundCheck = transform.Find("GroundCheck");
     }
 
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            jump = true;
+        }
+    }
 
     private void FixedUpdate()
     {
         float maxSpeed = 2;
         float moveForce = 300;
+        float jumpForce = 1000f;
 
         float h = Input.GetAxis("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(h));
@@ -47,6 +58,13 @@ public class Player : MonoBehaviour
         {
             Flip();
         }
+
+        if (jump)
+        {
+            animator.SetTrigger("Jump");
+            rigidbody.AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        }
     }
 
     void Flip()
@@ -56,5 +74,12 @@ public class Player : MonoBehaviour
         Vector3 scale = transform.localScale;
         scale.x *= -1;
         transform.localScale = scale;
+    }
+
+    bool isGrounded()
+    {
+        Debug.Log(LayerMask.NameToLayer("Ground"));
+        Debug.Log(groundCheck.position);
+        return Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
     }
 }
